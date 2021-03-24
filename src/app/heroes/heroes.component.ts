fe5d30d268b0508a987cc1173191af8d898038a5
Guillaume MAIANO <guillaume@maiano.fr>
2021-03-25 00:36:19 +0100

    L3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +11,17 @@ export class HeroesComponent implements OnInit {
 
 
   hero : Hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    console.log(hero.name);
+    this.selectedHero = hero;
   }
 
 }
