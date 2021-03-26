2f1c730838062589f17db27511b6b65c428e61c7
Guillaume MAIANO <guillaume@maiano.fr>
2021-03-26 17:12:53 +0100

    L6-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- so, NgModel lives here, huh.
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module'; // <-- so, NgModel lives here, huh.
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { MessagesComponent } from './messages/messages.component'; // <-- so, Ng
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
